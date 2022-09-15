29fca3d6df93731e180aa0ea6e9dff32381bc8a5
agwani <agwani1230@gmail.com>
2022-09-15 06:45:51 -0700

    Task commits

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - timing
+ */
+void jack_bauer(void)
+{
+int HourTen;
+int Minute;
+for (HourTen = 0; HourTen <= 23; HourTen++)
+{
+for (Minute = 0; Minute <= 59; Minute++)
+{
+_putchar('0' + (HourTen / 10));
+_putchar('0' + (HourTen % 10));
+_putchar(':');
+_putchar('0' + (Minute / 10));
+_putchar('0' + (Minute % 10));
+_putchar ('\n');
+}
+}
+}
